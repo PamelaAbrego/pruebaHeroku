b4442819e75271ebf43d1d42cdcbe4e61c3069bb
Pamela Abrego <pamelabregor@gmail.com>
2021-11-14 01:05:12 -0600

    -

--- app.py
+++ app.py
@@ -8,17 +8,12 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
-
 @app.route("/", methods=["GET", "POST"])
 def home():
-    if request.method == "GET":
-        return render_template("main.html")
-    else request.method == "POST":
-        celular ="+50379241086"
-        mensaje = "prueba"
-        # pywhatkit.sendwhatmsg_instantly(celular, mensaje,10,True,10)
-        return render_template("main.html")
-
+    celular ="+50379241086"
+    mensaje = "prueba"
+    # pywhatkit.sendwhatmsg_instantly(celular, mensaje,10,True,10)
+    return render_template("main.html")
         
 
 if __name__ == "__main__":
