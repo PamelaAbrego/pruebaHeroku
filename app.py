750bedee76f97eaeb1920e842322191e0d876c73
Pamela Abrego <pamelabregor@gmail.com>
2021-11-14 19:27:48 -0600

    -

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
-import pywhatkit
+#import pywhatkit
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -10,7 +10,7 @@ app.config["CORS_HEADERS"] = "Content-Type"
 def home():
     mensaje = "Hola"
     celular = "+50379241086"
-    pywhatkit.sendwhatmsg_instantly(celular, mensaje)
+    #pywhatkit.sendwhatmsg_instantly(celular, mensaje)
     return render_template("main.html")
         
 
